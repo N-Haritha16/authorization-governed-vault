b5fd2a399ad6d1526dd390d1d75b4b65f8fa8277
Haritha Nallamilli <harithanallamilli9702@gmail.com>
2025-12-28 16:19:19 +0530

    fix the errors

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "Compiling contracts..."
 npx hardhat compile
 
+echo "Waiting for blockchain to be ready..."
+sleep 10
+
 echo "Deploying contracts..."
 npx hardhat run scripts/deploy.js --network localhost
 
