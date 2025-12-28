11c75cd47177cba1033c65cb8456df621c501e4d
Haritha Nallamilli <harithanallamilli9702@gmail.com>
2025-12-28 19:33:28 +0530

    changes in hardhat url

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,12 @@
-// hardhat.config.js
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     localhost: {
+      url: "http://localhost:8545"
+    },
+    docker: {
       url: "http://blockchain:8545"
     }
   }
